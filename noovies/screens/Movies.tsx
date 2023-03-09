aa8dc47099bed3d1c434016604b2e7ad6dbd442a
beeeg55 <beeeg@naver.com>
2023-03-09 17:01:17 +0900

    typescript theme 적용

--- noovies/screens/Movies.tsx
+++ noovies/screens/Movies.tsx
@@ -1,19 +1,23 @@
 import react from "react";
 import { TouchableOpacity, Text } from "react-native";
 import styled from "styled-components/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 const Btn = styled.TouchableOpacity`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
 `;
 
 const Title = styled.Text`
-  color: ${(props) => (props.selected ? "blue" : "red")};
+  color: ${(props) => props.theme.textColor};
 `;
-const Movies = ({ navigation: { navigate } }) => (
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => (
   <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title selected={false}>Movies</Title>
+    <Title>Movies</Title>
   </Btn>
 );
 export default Movies;
