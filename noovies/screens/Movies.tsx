b8c20716ee236d83735db223c96a468812ee9b9a
beeeg55 <beeeg@naver.com>
2023-03-13 16:39:35 +0900

    trending movies 컴포넌트 구성, poster , 슬리아드 부분 컴포넌트화

--- noovies/screens/Movies.tsx
+++ noovies/screens/Movies.tsx
@@ -1,46 +1,91 @@
 import react, { useEffect, useState } from "react";
-import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Swiper from "react-native-web-swiper";
-import { makeImgPath } from "../utils";
-import { BlurView } from "@react-native-community/blur";
+import Swiper from "react-native-swiper";
+import { useColorScheme } from "react-native";
+import Slide from "../components/Slide";
+import { ScrollView } from "react-native-gesture-handler";
+import Poster from "../components/Poster";
 
 const API_KEY = "28e7b568189d701d8db3348e7f2622f1";
 
-const Container = styled.ScrollView``;
-
-const View = styled.View`
-  flex: 1;
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
 `;
+
 const Loader = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Title = styled.Text``;
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 // const SCREEN_HEIGHT  = Dimensions.get("window").height; 와 같은 의미다
 
+const ListTitle = styled.Text`
+  color: white;
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-top: 7px;
+  margin-bottom: 5px;
+`;
+const Votes = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 10px;
+`;
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+      )
+    ).json();
+    console.log(results);
+    setTrending(results);
+  };
+  const getUpComing = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
         `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
       )
     ).json();
-    console.log(results);
     setNowPlaying(results);
+  };
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpComing(), getNowPlaying()]);
     setLoading(false);
   };
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
   return loading ? (
     <Loader>
@@ -49,23 +94,46 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
-        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{
+          width: "100%",
+          marginBottom: 30,
+          height: SCREEN_HEIGHT / 4,
+        }}
       >
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            />
-            <BlurView intensity={80} style={StyleSheet.absoluteFill}>
-              <Title>{movie.original_title}</Title>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
+      <ListTitle>Trending Movies</ListTitle>
+      <TrendingScroll
+        contentContainerStyle={{ paddingLeft: 30 }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {trending.map((movie) => (
+          <Movie key={movie.id}>
+            <Poster path={movie.poster_path}></Poster>
+            <Title>
+              {movie.original_title.slice(0, 13)}
+              {movie.original_title.length > 13 ? "..." : null}
+            </Title>
+            <Votes>⭐️{movie.vote_average}/10</Votes>
+          </Movie>
+        ))}
+      </TrendingScroll>
     </Container>
   );
 };
