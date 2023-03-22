cd007a044670bda5fc9c5061e6a2a39f66ee2b34
beeeg55 <beeeg@naver.com>
2023-03-22 18:17:08 +0900

    navigate params 통해서 데이터 스크린간 넘기기

--- noovies/navigation/Stack.js
+++ noovies/navigation/Stack.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  useColorScheme,
+  StyleSheet,
+} from "react-native";
+import styled from "styled-components/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Detail from "../screens/Detail";
+import { BLACK_COLOR, DARK_GREY, LIGHT_GREY, YELLOW_COLOR } from "../colors";
 
 const NativeStack = createNativeStackNavigator();
 
-const Stack = () => (
-  <NativeStack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
-    <NativeStack.Screen name="Detail" component={Detail} />
-  </NativeStack.Navigator>
-);
+const Stack = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+      }}
+    >
+      <NativeStack.Screen name="Detail" component={Detail} />
+    </NativeStack.Navigator>
+  );
+};
 
 export default Stack;
