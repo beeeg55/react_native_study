13cc1de2d8353dfcfd6ffd3fa9681cd8dead9c5f
beeeg55 <beeeg@naver.com>
2023-03-27 13:20:02 +0900

    공유하기 기능 추가 , 동영상 콘텐츠 추가

--- noovies/screens/Detail.tsx
+++ noovies/screens/Detail.tsx
@@ -1,15 +1,18 @@
+import { Ionicons } from "@expo/vector-icons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect } from "react";
-import { View, Text, Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, Platform, Share } from "react-native";
 import { useQuery } from "react-query";
 import { parseMutationArgs } from "react-query/types/core/utils";
 import styled from "styled-components/native";
 import { Movie, moviesApi, TV, tvApi } from "../api";
 import { BLACK_COLOR } from "../colors";
+import Loader from "../components/Loader";
 import Poster from "../components/Poster";
 import { makeImgPath } from "../utils";
-
+import * as WebBrowser from "expo-web-browser";
+import { TouchableOpacity } from "react-native-gesture-handler";
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
@@ -38,36 +41,84 @@ const Overview = styled.Text`
   padding: 0px 20px;
 `;
 
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
+`;
+
+const Data = styled.View`
+  padding: 0px 20px;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const openYTLink = async (videoID) => {
+  const baseUrl = `http://m.youtube.com/watch?v=${videoID}`;
+  //await Linking.openURL(baseUrl);
+  await WebBrowser.openBrowserAsync(baseUrl);
+};
+
 type RootStackParamList = {
   Detail: Movie | TV;
 };
 
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
 const Detail = ({ navigation: { setOptions }, route: { params } }) => {
-  const { isLoading: moviesLoading, data: moviesData } = useQuery(
-    ["movies", params.id],
-    moviesApi.detail,
-    {
-      enabled: "original_title" in params,
-    }
+  const isMovie = "original_title" in params;
+  const { isLoading, data } = useQuery(
+    [isMovie ? "movies" : "tv", params.id],
+    isMovie ? moviesApi.detail : tvApi.detail
   );
-  const { isLoading: tvLoading, data: tvData } = useQuery(
-    ["tv", params.id],
-    tvApi.detail,
-    {
-      enabled: "original_name" in params,
+  const shareMedia = async () => {
+    const isAndroid = Platform.OS === "android";
+    const homepage = isMovie
+      ? `https://www.imdb.com/title/${data.imdb_id}/`
+      : data.homepage;
+    if (isAndroid) {
+      await Share.share({
+        message: `${params.overview}\nCheck it out: ${homepage}`,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
+    } else {
+      await Share.share({
+        url: homepage,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
     }
+  };
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share-outline" color="white" size={24} />
+    </TouchableOpacity>
   );
-  console.log("moviesData", moviesData);
-  console.log("tvData", tvData);
+
   useEffect(
     () =>
       setOptions({
         title: "original_title" in params ? "Movie" : "TV Show",
+        headerRight: () => <ShareButton />,
       }),
     []
   );
-
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
   return (
     <Container>
       <Header>
@@ -89,6 +140,15 @@ const Detail = ({ navigation: { setOptions }, route: { params } }) => {
         </Column>
       </Header>
       <Overview>{params.overview}</Overview>
+      {isLoading ? <Loader /> : null}
+      <Data>
+        {data?.videos?.results?.map((video) => (
+          <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+            <Ionicons name="logo-youtube" color="white" size={24} />
+            <BtnText>{video.name}</BtnText>
+          </VideoBtn>
+        ))}
+      </Data>
     </Container>
   );
 };
